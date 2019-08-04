ae89854c77e8c3a3de2b92d19e44d0665961682c
darenlarson <larson.daren@gmail.com>
2019-08-04 18:57:03 -0500

    don't remember

--- routes/habitsHelper.js
+++ routes/habitsHelper.js
@@ -21,18 +21,21 @@ module.exports = {
   },
 
   completeHabit: function(habitData) {
-    const { id, user_id, habit_name, last_completed, today, yesterday, rating, count, number } = habitData;
+    const { id, user_id, habit_name, habit_type, last_completed, last_value_added, today, yesterday, rating, count, number } = habitData;
     console.log(habitData);
 
+    // If habit data hasn't been submitted today, go ahead and submit it.
     if (last_completed !== today) {
       return db("habit_tracker")
         .insert({ habit_id: id, user_id, habit_name, date_completed: today, rating: rating, count: count, number: number })
         .then(() => {
           return db("habits")
             .where("id", id)
-            .update({ last_completed: today });
+            .update({ last_completed: today, last_value_added: last_value_added });
         });
-    } else {
+
+      // If habit data has been submitted today, and this is a 'normal' habit type, remove the habit record and update the last_completed date in the habits table
+    } else if (habit_type === 'normal') {
       return db("habit_tracker")
         .where({ habit_id: id, date_completed: today })
         .del()
@@ -41,6 +44,17 @@ module.exports = {
             .where("id", id)
             .update({ last_completed: yesterday });
         });
+
+      // If habit data has been submitted today and it isn't a 'normal' habit type, update the record that is already in the habit_tracker table
+    } else {
+      return db("habit_tracker")
+        .where({ habit_id: id, date_completed: today })
+        .update({ rating: rating, count: count, number: number })
+        .then(() => {
+          return db("habits")
+            .where("id", id)
+            .update({ last_value_added: last_value_added })
+        })
     }
   },
 
